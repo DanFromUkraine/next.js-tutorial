81e8d44980ef39f434bece3a28a289a392633cca
DanFromUkraine <ovsannikovdana91@gmail.com>
2024-10-27 14:05:31 +0200

    added suspense to some components in dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: undefined
+  },
 };
 
 export default nextConfig;
